e40b983130a7b9506d14d85cb133fb56b35e318e
yashaswa <sharmayashswa777@gmail.com>
2023-12-30 16:46:13 +0530

    signup login with password encryption

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+const apiRoutes = require('./routes/index')
 
 const {PORT} = require('./config/serverConfig');
 
 const prepareAndStartServer = ()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api' ,apiRoutes);
+
 app.listen(PORT , () => {
 console.log(`server started at ${PORT} `);
 })
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const {SALT }= require('../config/serverConfig');
+const bcrypt = require('bcrypt')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +36,11 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+    User.beforeCreate((user)=>{
+      const encryptedPassword = bcrypt.hashSync(user.password,SALT);
+      user.password = encryptedPassword;
+    })
+
   return User;
 };
